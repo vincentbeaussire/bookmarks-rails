c741564cc91462faa4308574724cf46b93995834
Vincent Beaussire <vincentpolska@gmail.com>
2023-10-30 15:18:01 +0100

    db table

--- db/migrate/20231030135154_create_books.rb
+++ db/migrate/20231030135154_create_books.rb
@@ -1,7 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-
+      t.string :title
+      t.string :author
+      t.string :description
+      t.string :publisher
+      t.integer :weeks_on_list
+      t.integer :rank_this_week
       t.timestamps
     end
   end
